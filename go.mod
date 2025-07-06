daad21b0f8ccfa8a6c64356f12eca45857d17af6
Johnathan W <j7n.w4r@gmail.com>
2025-07-05 20:00:32 -0400

    basic implementation

--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module tcp-chat-server-go
 
 go 1.24.4
 
-require (
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.9.1 // indirect
-	github.com/spf13/pflag v1.0.6 // indirect
-)
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"github.com/google/uuid"
 	"log"
 	"net"
+	"sync"
 )
 
-var userChannelMap = make(map[string]chan string)
+type Channels struct {
+	mutex          sync.Mutex
+	userChannelMap map[uuid.UUID]chan string
+}
+
+var channels Channels
 
 func main() {
 	log.Printf("tcp_chat_server_go starting...")
+	channels.userChannelMap = make(map[uuid.UUID]chan string)
 
 	listener, err := net.Listen("tcp", ":23234")
 	if err != nil {
@@ -17,6 +27,8 @@ func main() {
 	defer listener.Close()
 
 	// Listing Loop
+	appCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	for {
 		conn, connErr := listener.Accept()
 		if connErr != nil {
@@ -24,13 +36,74 @@ func main() {
 			continue
 		}
 
-		go handleConnection(conn)
+		go handleConnection(appCtx, conn)
 	}
 }
 
-func handleConnection(conn net.Conn) {
-	log.Printf("tcp_chat_server_go handling connection from %v", conn.RemoteAddr())
-	conn.Write([]byte("testing"))
-
+func handleConnection(ctx context.Context, conn net.Conn) {
 	defer conn.Close()
+	connID := uuid.New()
+
+	channels.mutex.Lock()
+	channels.userChannelMap[connID] = make(chan string)
+	channels.mutex.Unlock()
+
+	log.Printf("tcp_chat_server_go handling connection from %v", conn.RemoteAddr())
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go handleRead(ctx, &wg, conn, connID)
+	go handleWrite(ctx, &wg, conn, connID)
+	wg.Wait()
+}
+
+func handleRead(ctx context.Context, wg *sync.WaitGroup, conn net.Conn, connID uuid.UUID) {
+	defer wg.Done()
+
+	buf := make([]byte, 1024)
+	for {
+		if ctxErr := ctx.Err(); ctxErr != nil {
+			if !errors.Is(ctxErr, context.Canceled) {
+				log.Printf("tcp_chat_server_go read error: %v", ctxErr)
+			}
+			return
+		}
+
+		n, readErr := conn.Read(buf)
+		if readErr != nil {
+			log.Printf("tcp_chat_server_go failed to read: %v", readErr)
+			return
+		}
+		message := string(buf[:n])
+
+		channels.mutex.Lock()
+		for userID, channel := range channels.userChannelMap {
+			if userID == connID {
+				continue
+			}
+			channel <- message
+		}
+		channels.mutex.Unlock()
+	}
+}
+
+func handleWrite(ctx context.Context, wg *sync.WaitGroup, conn net.Conn, connID uuid.UUID) {
+	defer wg.Done()
+	channels.mutex.Lock()
+	connChannel := channels.userChannelMap[connID]
+	channels.mutex.Unlock()
+	
+	for {
+		if ctxErr := ctx.Err(); ctxErr != nil {
+			if !errors.Is(ctxErr, context.Canceled) {
+				log.Printf("tcp_chat_server_go read error: %v", ctxErr)
+			}
+			return
+		}
+		message := <-connChannel
+
+		_, err := conn.Write([]byte(message))
+		if err != nil {
+			log.Printf("tcp_chat_server_go failed to write: %v", message)
+		}
+	}
 }
